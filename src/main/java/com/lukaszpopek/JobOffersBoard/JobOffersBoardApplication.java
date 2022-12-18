b80900fd920a59bdf4ab3ad8db8346cddc96e81b
lukasz_p <merc.angel@hotmail.com>
2022-12-18 14:39:27 +0100

    Initial commit

---
+++ src/main/java/com/lukaszpopek/JobOffersBoard/JobOffersBoardApplication.java
@@ -0,0 +1,13 @@
+package com.lukaszpopek.JobOffersBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobOffersBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobOffersBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lukaszpopek/JobOffersBoard/JobOffersBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukaszpopek.JobOffersBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobOffersBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
